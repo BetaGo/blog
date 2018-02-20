a3dd3287fdf3c573397fc898b6d7774964aa6f26
BetaGo <gouwantong@live.com>
2018-02-20 15:08:35 +0800

    test

---
+++ src/templates/post.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export default class BlogPostTemplate extends Component {
+  render() {
+      const post = this.props.data.markdownRemark;
+    return (
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{__html: post.html}} />
+      </div>
+    )
+  }
+}
+
+export const pageQuery = graphql`
+    query BlogPostBySlug($slug: String!) {
+        markdownRemark(fields: { slug: { eq: $slug } }) {
+            html
+            frontmatter {
+                title
+            }
+        }
+    }
+`
